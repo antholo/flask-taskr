44043e81c678cf858a53b8a970682a8bf7e45917
antholo <blooker@gmail.com>
2014-11-11 11:34:07 -0600

    updated files for SQLAlchemy from sqlite3

---
+++ app/db_create.py
@@ -0,0 +1,15 @@
+# db_create.py
+
+from views import db
+from models import Task
+from datetime import date
+
+# create the database and the db table
+db.create_all()
+
+# insert data
+db.session.add(Task("Finish this tutorial", date(2014, 11, 14), 10, 1))
+db.session.add(Task("Finish Real Python", date(2014, 11, 14), 10, 1))
+
+# commit the changes
+db.session.commit()
