71106d2f8a8f1cac2c511ddaf17f39b67f626a90
antholo <blooker@gmail.com>
2014-11-21 13:56:31 -0600

    finished testing chapter

--- app/views.py
+++ app/views.py
@@ -1,18 +1,28 @@
 # views.py
 
+#################
+#### imports ####
+#################
+
 from flask import Flask, flash, redirect, render_template, request,\
-    session, url_for, g
+    session, url_for
 from functools import wraps
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 # Add custom form for this app
-from forms import AddTaskForm
+from forms import AddTaskForm, RegisterForm, LoginForm
+import datetime
+
+################
+#### config ####
+################
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # Model for this app
-from models import Task
+from models import Task, User
 
 
 def login_required(test):
@@ -27,25 +37,42 @@ def login_required(test):
 
 
 @app.route('/logout/')
+@login_required
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('You are logged out. Bye. :(')
     return redirect(url_for('login'))
 
 
-@app.route('/', methods=['Get', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] \
-                or request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid Credentials. Please try again.'
-            return render_template('login.html', error=error)
+        if form.validate_on_submit():
+            u = User.query.filter_by(
+                name=request.form['name'],
+                password=request.form['password']).first()
+            if u is None:
+                error = 'Invalid username or password.'
+                return render_template(
+                    "login.html",
+                    form=form,
+                    error=error)
+            else:
+                session['logged_in'] = True
+                session['user_id'] = u.id
+                flash('You are logged in. Go crazy.')
+                return redirect(url_for('tasks'))
         else:
-            session['logged_in'] = True
-            return redirect(url_for('tasks'))
+            return render_template(
+                "login.html",
+                form=form,
+                error=error)
+
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template("login.html", form=form, error=error)
 
 
 # View tasks
@@ -66,17 +93,25 @@ def tasks():
 @app.route('/add/', methods=['GET', 'POST'])
 @login_required
 def new_task():
+    import datetime
+    error=None
     form = AddTaskForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
             new_task = Task(form.name.data,
                             form.due_date.data,
                             form.priority.data,
-                            '1')
+                            datetime.datetime.utcnow(),
+                            '1',
+                            session['user_id'])
             db.session.add(new_task)
             db.session.commit()
             flash('New entry was successfully posted. Thanks')
-    return redirect(url_for('tasks'))
+            return redirect(url_for('tasks'))
+        else:
+            return render_template('tasks.html', form=form, error=error)
+    if request.method == 'GET':
+        return render_template('tasks.html', form=form)
 
 
 # Mark tasks as complete:
@@ -99,3 +134,35 @@ def delete_entry(task_id):
     db.session.commit()
     flash('The task was deleted. Why not add a new one?')
     return redirect(url_for('tasks'))
+
+
+# User Registration:
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data)
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'Oh no! That username and/or email already exist. Please try again.'
+                return render_template('register.html', form=form, error=error)
+        else:
+            return render_template('register.html', form=form, error=error)
+    if request.method == 'GET':
+        return render_template('register.html', form=form)
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the {0} field - {1}" .format(
+                getattr(form, field).label.text, error), 'error')
